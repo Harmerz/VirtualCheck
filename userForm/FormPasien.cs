eff72847f66479f30fa25816cea3e5ad59ff2977
krisnaarianta <krisnaarianta@gmail.com>
2023-11-16 11:33:59 +0700

    feat: last update connection

--- userForm/FormPasien.cs
+++ userForm/FormPasien.cs
@@ -14,7 +14,7 @@ namespace userForm
         }
 
         private NpgsqlConnection conn;
-        string connstring = "Host=20.205.32.88;Port=5432;Username=krisna;Password=admin123;Database=junpro_virtualcheck";
+        string connstring = "Host=20.205.32.88;Port=5432;Username=postgres;Password=;Database=junpro_virtualcheck";
 
         public DataTable dt;
         public static NpgsqlCommand cmd;
@@ -43,12 +43,12 @@ namespace userForm
             try
             {
                 conn.Open();
-                sql = @"select * from insert_pasien(:_name, :_age, :_address, :_sex :_telp, :_keluhan)";
+                sql = @"select * from insert_pasien(:_name, :_age, :_address, :_telp, :_keluhan)";
                 cmd = new NpgsqlCommand(sql, conn);
                 cmd.Parameters.AddWithValue("_name", txtNama.Text);
                 cmd.Parameters.AddWithValue("_age", txtUmur.Text);
                 cmd.Parameters.AddWithValue("_address", txtAlamat.Text);
-                cmd.Parameters.AddWithValue("_sex", cbKelamin.Text);
+                //cmd.Parameters.AddWithValue("_sex", cbKelamin.Text);
                 cmd.Parameters.AddWithValue("_telp", txtTelp.Text);
                 cmd.Parameters.AddWithValue("_keluhan", txtKeluhan.Text);
 
@@ -57,7 +57,7 @@ namespace userForm
                     MessageBox.Show("Data Users Berhasil diinputkan", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     conn.Close();
                     button1.PerformClick();
-                    txtNama.Text = txtUmur.Text = txtAlamat.Text = txtTelp.Text = txtKeluhan.Text = cbKelamin.Text = null;
+                    txtNama.Text = txtUmur.Text = txtAlamat.Text = txtTelp.Text = txtKeluhan.Text = null;
                 }
             }
             catch (Exception ex)
