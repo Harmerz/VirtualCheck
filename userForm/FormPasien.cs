32b57df7a10fc1120cdb9b17afc4a90e530228ad
krisnaarianta <krisnaarianta@gmail.com>
2023-11-16 08:13:37 +0700

    feat: connection to db in form pasien

--- userForm/FormPasien.cs
+++ userForm/FormPasien.cs
@@ -1,3 +1,6 @@
+using Npgsql;
+
+
 namespace userForm
 {
     public partial class FormPasien : Form
@@ -5,7 +8,12 @@ namespace userForm
         public FormPasien()
         {
             InitializeComponent();
+            conn = new NpgsqlConnection(connstring);
         }
+
+        private NpgsqlConnection conn;
+        string connstring = "Host=http://20.205.32.88/;Port=5432;Username=postgres;Password=;Database=junpro_virtualcheck";
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -23,6 +31,11 @@ namespace userForm
             this.Close();
             FormAwal formAwal = new FormAwal();
             formAwal.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
