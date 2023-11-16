8ac5b67ca1cafe23e7aa9f6f32db3149ef7abd24
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-11-16 12:30:13 +0700

    feat : connection insert SQL

--- userForm/FormPasien.Designer.cs
+++ userForm/FormPasien.Designer.cs
@@ -47,9 +47,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(531, 1022);
+            button1.Location = new Point(286, 479);
+            button1.Margin = new Padding(2, 1, 2, 1);
             button1.Name = "button1";
-            button1.Size = new Size(310, 46);
+            button1.Size = new Size(167, 22);
             button1.TabIndex = 0;
             button1.Text = "Submit";
             button1.UseVisualStyleBackColor = true;
@@ -57,17 +58,19 @@
             // 
             // txtNama
             // 
-            txtNama.Location = new Point(654, 180);
+            txtNama.Location = new Point(352, 84);
+            txtNama.Margin = new Padding(2, 1, 2, 1);
             txtNama.Name = "txtNama";
-            txtNama.Size = new Size(414, 39);
+            txtNama.Size = new Size(225, 23);
             txtNama.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(350, 180);
+            label1.Location = new Point(188, 84);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(77, 32);
+            label1.Size = new Size(39, 15);
             label1.TabIndex = 3;
             label1.Text = "Nama";
             label1.Click += label1_Click;
@@ -75,102 +78,116 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(350, 262);
+            label2.Location = new Point(188, 123);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(73, 32);
+            label2.Size = new Size(37, 15);
             label2.TabIndex = 4;
             label2.Text = "Umur";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(350, 347);
+            label3.Location = new Point(188, 163);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(88, 32);
+            label3.Size = new Size(45, 15);
             label3.TabIndex = 5;
             label3.Text = "Alamat";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(350, 431);
+            label4.Location = new Point(188, 202);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(159, 32);
+            label4.Size = new Size(78, 15);
             label4.TabIndex = 6;
             label4.Text = "Jenis Kelamin";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(350, 512);
+            label5.Location = new Point(188, 240);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(58, 32);
+            label5.Size = new Size(28, 15);
             label5.TabIndex = 7;
             label5.Text = "Telp";
             // 
             // txtUmur
             // 
-            txtUmur.Location = new Point(654, 262);
+            txtUmur.Location = new Point(352, 123);
+            txtUmur.Margin = new Padding(2, 1, 2, 1);
             txtUmur.Name = "txtUmur";
-            txtUmur.Size = new Size(414, 39);
+            txtUmur.Size = new Size(225, 23);
             txtUmur.TabIndex = 8;
             // 
             // txtAlamat
             // 
-            txtAlamat.Location = new Point(654, 347);
+            txtAlamat.Location = new Point(352, 163);
+            txtAlamat.Margin = new Padding(2, 1, 2, 1);
             txtAlamat.Name = "txtAlamat";
-            txtAlamat.Size = new Size(414, 39);
+            txtAlamat.Size = new Size(225, 23);
             txtAlamat.TabIndex = 9;
             // 
             // txtTelp
             // 
-            txtTelp.Location = new Point(654, 512);
+            txtTelp.Location = new Point(352, 240);
+            txtTelp.Margin = new Padding(2, 1, 2, 1);
             txtTelp.Name = "txtTelp";
-            txtTelp.Size = new Size(414, 39);
+            txtTelp.Size = new Size(225, 23);
             txtTelp.TabIndex = 10;
             // 
             // cbKelamin
             // 
             cbKelamin.FormattingEnabled = true;
-            cbKelamin.Location = new Point(654, 428);
+            cbKelamin.Items.AddRange(new object[] { "Laki-laki", "Perempuan" });
+            cbKelamin.Location = new Point(352, 201);
+            cbKelamin.Margin = new Padding(2, 1, 2, 1);
             cbKelamin.Name = "cbKelamin";
-            cbKelamin.Size = new Size(414, 40);
+            cbKelamin.Size = new Size(225, 23);
             cbKelamin.TabIndex = 11;
+            cbKelamin.SelectedIndexChanged += cbKelamin_SelectedIndexChanged;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(350, 601);
+            label6.Location = new Point(188, 282);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(101, 32);
+            label6.Size = new Size(50, 15);
             label6.TabIndex = 12;
             label6.Text = "Keluhan";
             label6.Click += label6_Click;
             // 
             // txtKeluhan
             // 
-            txtKeluhan.Location = new Point(654, 601);
+            txtKeluhan.Location = new Point(352, 282);
+            txtKeluhan.Margin = new Padding(2, 1, 2, 1);
             txtKeluhan.Multiline = true;
             txtKeluhan.Name = "txtKeluhan";
             txtKeluhan.ScrollBars = ScrollBars.Vertical;
-            txtKeluhan.Size = new Size(414, 259);
+            txtKeluhan.Size = new Size(225, 124);
             txtKeluhan.TabIndex = 13;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(531, 58);
+            label7.Location = new Point(286, 27);
+            label7.Margin = new Padding(2, 0, 2, 0);
             label7.Name = "label7";
-            label7.Size = new Size(310, 32);
+            label7.Size = new Size(152, 15);
             label7.TabIndex = 14;
             label7.Text = "Form Data Diri dan Keluhan";
             label7.TextAlign = ContentAlignment.TopCenter;
             // 
             // btnLogOutPasien
             // 
-            btnLogOutPasien.Location = new Point(531, 1116);
+            btnLogOutPasien.Location = new Point(286, 523);
+            btnLogOutPasien.Margin = new Padding(2, 1, 2, 1);
             btnLogOutPasien.Name = "btnLogOutPasien";
-            btnLogOutPasien.Size = new Size(310, 46);
+            btnLogOutPasien.Size = new Size(167, 22);
             btnLogOutPasien.TabIndex = 15;
             btnLogOutPasien.Text = "Log Out";
             btnLogOutPasien.UseVisualStyleBackColor = true;
@@ -178,9 +195,9 @@
             // 
             // FormPasien
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1374, 1245);
+            ClientSize = new Size(740, 497);
             Controls.Add(btnLogOutPasien);
             Controls.Add(label7);
             Controls.Add(txtKeluhan);
@@ -196,6 +213,7 @@
             Controls.Add(label1);
             Controls.Add(txtNama);
             Controls.Add(button1);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "FormPasien";
             Text = "Form1";
             ResumeLayout(false);
--- userForm/FormPasien.cs
+++ userForm/FormPasien.cs
@@ -14,7 +14,7 @@ namespace userForm
         }
 
         private NpgsqlConnection conn;
-        string connstring = "Host=20.205.32.88;Port=5432;Username=postgres;Password=;Database=junpro_virtualcheck";
+        string connstring = "Host=20.205.32.88;Port=5432;Username=postgres;Database=junpro_virtualcheck";
 
         public DataTable dt;
         public static NpgsqlCommand cmd;
@@ -43,27 +43,34 @@ namespace userForm
             try
             {
                 conn.Open();
-                sql = @"select * from insert_pasien(:_name, :_age, :_address, :_telp, :_keluhan)";
+                sql = @"select * from insert_pasien(:_name, :_age, :_address, :_sex, :_telp, :_keluhan)";
                 cmd = new NpgsqlCommand(sql, conn);
+                Boolean sex = cbKelamin.Text == "Laki-laki";
                 cmd.Parameters.AddWithValue("_name", txtNama.Text);
-                cmd.Parameters.AddWithValue("_age", txtUmur.Text);
+                cmd.Parameters.AddWithValue("_age", int.Parse(txtUmur.Text));
                 cmd.Parameters.AddWithValue("_address", txtAlamat.Text);
-                //cmd.Parameters.AddWithValue("_sex", cbKelamin.Text);
+                cmd.Parameters.AddWithValue("_sex", sex);
                 cmd.Parameters.AddWithValue("_telp", txtTelp.Text);
                 cmd.Parameters.AddWithValue("_keluhan", txtKeluhan.Text);
-
                 if ((int)cmd.ExecuteScalar() == 1)
                 {
                     MessageBox.Show("Data Users Berhasil diinputkan", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    conn.Close();
-                    button1.PerformClick();
+                    conn.Close();   
                     txtNama.Text = txtUmur.Text = txtAlamat.Text = txtTelp.Text = txtKeluhan.Text = null;
+                }else
+                {
+                    MessageBox.Show("Data gagal di inputkan", "We Are Sorry", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message, "Insert FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void cbKelamin_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
