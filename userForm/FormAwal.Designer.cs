f40f7da636859ce644643879a8d324965db19b83
krisnaarianta <krisnaarianta@gmail.com>
2023-12-03 21:42:51 +0700

    fix: nama

--- userForm/FormAwal.Designer.cs
+++ userForm/FormAwal.Designer.cs
@@ -53,10 +53,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 16.125F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(83, 9);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(154, 19);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(151, 30);
+            label1.Size = new Size(293, 59);
             label1.TabIndex = 0;
             label1.Text = "VirtualCheck";
             label1.Click += label1_Click;
@@ -66,10 +66,10 @@
             button1.BackColor = Color.Firebrick;
             button1.Cursor = Cursors.Hand;
             button1.ForeColor = SystemColors.Control;
-            button1.Location = new Point(337, 172);
-            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Location = new Point(626, 367);
+            button1.Margin = new Padding(4, 2, 4, 2);
             button1.Name = "button1";
-            button1.Size = new Size(180, 45);
+            button1.Size = new Size(334, 96);
             button1.TabIndex = 1;
             button1.Text = "Doctor";
             button1.UseVisualStyleBackColor = false;
@@ -80,10 +80,10 @@
             button2.BackColor = Color.IndianRed;
             button2.Cursor = Cursors.Hand;
             button2.ForeColor = SystemColors.Control;
-            button2.Location = new Point(337, 234);
-            button2.Margin = new Padding(2, 1, 2, 1);
+            button2.Location = new Point(626, 499);
+            button2.Margin = new Padding(4, 2, 4, 2);
             button2.Name = "button2";
-            button2.Size = new Size(180, 45);
+            button2.Size = new Size(334, 96);
             button2.TabIndex = 2;
             button2.Text = "Patient";
             button2.UseVisualStyleBackColor = false;
@@ -93,10 +93,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(403, 140);
-            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Location = new Point(748, 299);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(139, 21);
+            label2.Size = new Size(276, 45);
             label2.TabIndex = 3;
             label2.Text = "Please Select One";
             label2.Click += label2_Click;
@@ -105,9 +105,10 @@
             // 
             pictureBox1.ErrorImage = null;
             pictureBox1.Image = Properties.Resources.VC2;
-            pictureBox1.Location = new Point(15, 9);
+            pictureBox1.Location = new Point(28, 19);
+            pictureBox1.Margin = new Padding(6, 6, 6, 6);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(63, 37);
+            pictureBox1.Size = new Size(117, 79);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
@@ -116,9 +117,10 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources._10130;
-            pictureBox2.Location = new Point(-8, 77);
+            pictureBox2.Location = new Point(-15, 164);
+            pictureBox2.Margin = new Padding(6, 6, 6, 6);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(355, 234);
+            pictureBox2.Size = new Size(659, 499);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 5;
             pictureBox2.TabStop = false;
@@ -127,9 +129,10 @@
             // 
             pictureBox3.BackColor = Color.Transparent;
             pictureBox3.Image = Properties.Resources.doctor;
-            pictureBox3.Location = new Point(522, 172);
+            pictureBox3.Location = new Point(969, 367);
+            pictureBox3.Margin = new Padding(6, 6, 6, 6);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(48, 43);
+            pictureBox3.Size = new Size(89, 92);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.TabIndex = 7;
             pictureBox3.TabStop = false;
@@ -138,9 +141,10 @@
             // 
             pictureBox4.BackColor = Color.Transparent;
             pictureBox4.Image = Properties.Resources.medical;
-            pictureBox4.Location = new Point(522, 236);
+            pictureBox4.Location = new Point(969, 503);
+            pictureBox4.Margin = new Padding(6, 6, 6, 6);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(48, 43);
+            pictureBox4.Size = new Size(89, 92);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 8;
             pictureBox4.TabStop = false;
@@ -150,36 +154,40 @@
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(373, 98);
+            label3.Location = new Point(693, 209);
+            label3.Margin = new Padding(6, 0, 6, 0);
             label3.Name = "label3";
-            label3.Size = new Size(187, 32);
+            label3.Size = new Size(373, 65);
             label3.TabIndex = 9;
             label3.Text = "Welcome Back!";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 325);
+            label4.Location = new Point(22, 693);
+            label4.Margin = new Padding(6, 0, 6, 0);
             label4.Name = "label4";
-            label4.Size = new Size(72, 15);
+            label4.Size = new Size(142, 32);
             label4.TabIndex = 10;
             label4.Text = "Haikal Hilmi";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(12, 340);
+            label5.Location = new Point(22, 725);
+            label5.Margin = new Padding(6, 0, 6, 0);
             label5.Name = "label5";
-            label5.Size = new Size(180, 15);
+            label5.Size = new Size(352, 32);
             label5.TabIndex = 11;
-            label5.Text = "Johannes de Britto Krisna Arianta";
+            label5.Text = "Johanes de Britto Krisna Arianta";
             // 
             // pictureBox5
             // 
             pictureBox5.Image = Properties.Resources.Hiasan_1;
-            pictureBox5.Location = new Point(455, 0);
+            pictureBox5.Location = new Point(845, 0);
+            pictureBox5.Margin = new Padding(6, 6, 6, 6);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(164, 118);
+            pictureBox5.Size = new Size(305, 252);
             pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox5.TabIndex = 12;
             pictureBox5.TabStop = false;
@@ -188,19 +196,20 @@
             // 
             pictureBox6.ErrorImage = Properties.Resources.Hiasan_2;
             pictureBox6.Image = Properties.Resources.Hiasan_2;
-            pictureBox6.Location = new Point(479, 236);
+            pictureBox6.Location = new Point(890, 503);
+            pictureBox6.Margin = new Padding(6, 6, 6, 6);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(180, 153);
+            pictureBox6.Size = new Size(334, 326);
             pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox6.TabIndex = 13;
             pictureBox6.TabStop = false;
             // 
             // FormAwal
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(617, 364);
+            ClientSize = new Size(1146, 777);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
@@ -214,7 +223,7 @@
             Controls.Add(pictureBox1);
             Controls.Add(pictureBox5);
             Controls.Add(pictureBox6);
-            Margin = new Padding(2, 1, 2, 1);
+            Margin = new Padding(4, 2, 4, 2);
             Name = "FormAwal";
             Text = "FormAwal";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
