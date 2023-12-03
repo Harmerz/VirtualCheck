c5bb27f79959f7f4ed9c045bece233f4871d4af2
krisnaarianta <krisnaarianta@gmail.com>
2023-12-03 21:28:12 +0700

    fix: appointment UI

--- userForm/Appoiment.Designer.cs
+++ userForm/Appoiment.Designer.cs
@@ -60,9 +60,10 @@
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.Hiasan_3;
-            pictureBox3.Location = new Point(-7, 569);
+            pictureBox3.Location = new Point(-13, 1214);
+            pictureBox3.Margin = new Padding(6, 6, 6, 6);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(265, 195);
+            pictureBox3.Size = new Size(492, 416);
             pictureBox3.TabIndex = 45;
             pictureBox3.TabStop = false;
             // 
@@ -70,10 +71,10 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(497, 46);
-            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Location = new Point(923, 98);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(175, 30);
+            label7.Size = new Size(348, 57);
             label7.TabIndex = 48;
             label7.Text = "Your Appoiment";
             label7.TextAlign = ContentAlignment.TopCenter;
@@ -82,10 +83,10 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI Black", 16.125F, FontStyle.Bold, GraphicsUnit.Point);
-            label8.Location = new Point(80, 3);
-            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Location = new Point(149, 6);
+            label8.Margin = new Padding(4, 0, 4, 0);
             label8.Name = "label8";
-            label8.Size = new Size(151, 30);
+            label8.Size = new Size(293, 59);
             label8.TabIndex = 41;
             label8.Text = "VirtualCheck";
             // 
@@ -93,9 +94,10 @@
             // 
             pictureBox6.ErrorImage = null;
             pictureBox6.Image = Properties.Resources.VC2;
-            pictureBox6.Location = new Point(12, 3);
+            pictureBox6.Location = new Point(22, 6);
+            pictureBox6.Margin = new Padding(6, 6, 6, 6);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(63, 37);
+            pictureBox6.Size = new Size(117, 79);
             pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox6.TabIndex = 42;
             pictureBox6.TabStop = false;
@@ -103,9 +105,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.appoiment_3;
-            pictureBox1.Location = new Point(12, 46);
+            pictureBox1.Location = new Point(22, 98);
+            pictureBox1.Margin = new Padding(6, 6, 6, 6);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(450, 539);
+            pictureBox1.Size = new Size(836, 1150);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 43;
             pictureBox1.TabStop = false;
@@ -114,9 +117,10 @@
             // 
             pictureBox2.ErrorImage = Properties.Resources.Hiasan_4;
             pictureBox2.Image = Properties.Resources.Hiasan_4;
-            pictureBox2.Location = new Point(570, 392);
+            pictureBox2.Location = new Point(1059, 836);
+            pictureBox2.Margin = new Padding(6, 6, 6, 6);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(315, 287);
+            pictureBox2.Size = new Size(585, 612);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 44;
             pictureBox2.TabStop = false;
@@ -124,9 +128,10 @@
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.Hiasan_1;
-            pictureBox4.Location = new Point(714, -22);
+            pictureBox4.Location = new Point(1326, -47);
+            pictureBox4.Margin = new Padding(6, 6, 6, 6);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(141, 116);
+            pictureBox4.Size = new Size(262, 247);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 46;
             pictureBox4.TabStop = false;
@@ -134,9 +139,10 @@
             // pictureBox5
             // 
             pictureBox5.Image = Properties.Resources.Hiasan_2;
-            pictureBox5.Location = new Point(747, 154);
+            pictureBox5.Location = new Point(1387, 329);
+            pictureBox5.Margin = new Padding(6, 6, 6, 6);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(152, 184);
+            pictureBox5.Size = new Size(282, 393);
             pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox5.TabIndex = 47;
             pictureBox5.TabStop = false;
@@ -145,9 +151,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(503, 103);
+            label1.Location = new Point(934, 220);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(44, 17);
+            label1.Size = new Size(88, 36);
             label1.TabIndex = 49;
             label1.Text = "Name";
             // 
@@ -155,9 +162,10 @@
             // 
             txtName.AutoSize = true;
             txtName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtName.Location = new Point(503, 124);
+            txtName.Location = new Point(934, 265);
+            txtName.Margin = new Padding(6, 0, 6, 0);
             txtName.Name = "txtName";
-            txtName.Size = new Size(52, 21);
+            txtName.Size = new Size(105, 45);
             txtName.TabIndex = 50;
             txtName.Text = "Name";
             // 
@@ -165,9 +173,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(503, 158);
+            label2.Location = new Point(934, 337);
+            label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(50, 17);
+            label2.Size = new Size(98, 36);
             label2.TabIndex = 51;
             label2.Text = "Doctor";
             // 
@@ -175,9 +184,10 @@
             // 
             txtDoctor.AutoSize = true;
             txtDoctor.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtDoctor.Location = new Point(503, 175);
+            txtDoctor.Location = new Point(934, 373);
+            txtDoctor.Margin = new Padding(6, 0, 6, 0);
             txtDoctor.Name = "txtDoctor";
-            txtDoctor.Size = new Size(57, 21);
+            txtDoctor.Size = new Size(117, 45);
             txtDoctor.TabIndex = 52;
             txtDoctor.Text = "Doctor";
             // 
@@ -185,9 +195,10 @@
             // 
             txtHospital.AutoSize = true;
             txtHospital.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtHospital.Location = new Point(503, 229);
+            txtHospital.Location = new Point(934, 489);
+            txtHospital.Margin = new Padding(6, 0, 6, 0);
             txtHospital.Name = "txtHospital";
-            txtHospital.Size = new Size(67, 21);
+            txtHospital.Size = new Size(138, 45);
             txtHospital.TabIndex = 54;
             txtHospital.Text = "Hospital";
             // 
@@ -195,9 +206,10 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label5.Location = new Point(503, 208);
+            label5.Location = new Point(934, 444);
+            label5.Margin = new Padding(6, 0, 6, 0);
             label5.Name = "label5";
-            label5.Size = new Size(60, 17);
+            label5.Size = new Size(116, 36);
             label5.TabIndex = 53;
             label5.Text = "Hospital";
             // 
@@ -205,9 +217,10 @@
             // 
             txtDay.AutoSize = true;
             txtDay.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtDay.Location = new Point(503, 280);
+            txtDay.Location = new Point(934, 597);
+            txtDay.Margin = new Padding(6, 0, 6, 0);
             txtDay.Name = "txtDay";
-            txtDay.Size = new Size(37, 21);
+            txtDay.Size = new Size(73, 45);
             txtDay.TabIndex = 56;
             txtDay.Text = "Day";
             // 
@@ -215,9 +228,10 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label9.Location = new Point(503, 259);
+            label9.Location = new Point(934, 553);
+            label9.Margin = new Padding(6, 0, 6, 0);
             label9.Name = "label9";
-            label9.Size = new Size(32, 17);
+            label9.Size = new Size(62, 36);
             label9.TabIndex = 55;
             label9.Text = "Day";
             // 
@@ -225,9 +239,10 @@
             // 
             txtTime.AutoSize = true;
             txtTime.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtTime.Location = new Point(503, 338);
+            txtTime.Location = new Point(934, 721);
+            txtTime.Margin = new Padding(6, 0, 6, 0);
             txtTime.Name = "txtTime";
-            txtTime.Size = new Size(44, 21);
+            txtTime.Size = new Size(90, 45);
             txtTime.TabIndex = 58;
             txtTime.Text = "Time";
             // 
@@ -235,9 +250,10 @@
             // 
             label11.AutoSize = true;
             label11.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label11.Location = new Point(503, 317);
+            label11.Location = new Point(934, 676);
+            label11.Margin = new Padding(6, 0, 6, 0);
             label11.Name = "label11";
-            label11.Size = new Size(39, 17);
+            label11.Size = new Size(75, 36);
             label11.TabIndex = 57;
             label11.Text = "Time";
             // 
@@ -246,14 +262,14 @@
             button1.BackColor = Color.Firebrick;
             button1.Cursor = Cursors.Hand;
             button1.FlatStyle = FlatStyle.Popup;
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Font = new Font("Segoe UI Black", 13.875F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = SystemColors.Control;
-            button1.Location = new Point(497, 542);
-            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Location = new Point(923, 1156);
+            button1.Margin = new Padding(4, 2, 4, 2);
             button1.Name = "button1";
-            button1.Size = new Size(286, 43);
+            button1.Size = new Size(531, 92);
             button1.TabIndex = 59;
-            button1.Text = "OKE";
+            button1.Text = "Okay";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
@@ -261,9 +277,10 @@
             // 
             txtKeluhan.AutoSize = true;
             txtKeluhan.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtKeluhan.Location = new Point(503, 392);
+            txtKeluhan.Location = new Point(934, 836);
+            txtKeluhan.Margin = new Padding(6, 0, 6, 0);
             txtKeluhan.Name = "txtKeluhan";
-            txtKeluhan.Size = new Size(66, 21);
+            txtKeluhan.Size = new Size(134, 45);
             txtKeluhan.TabIndex = 61;
             txtKeluhan.Text = "Keluhan";
             // 
@@ -271,18 +288,19 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(503, 371);
+            label4.Location = new Point(934, 791);
+            label4.Margin = new Padding(6, 0, 6, 0);
             label4.Name = "label4";
-            label4.Size = new Size(58, 17);
+            label4.Size = new Size(115, 36);
             label4.TabIndex = 60;
             label4.Text = "Keluhan";
             // 
             // Appoiment
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(846, 639);
+            ClientSize = new Size(1571, 1363);
             Controls.Add(txtKeluhan);
             Controls.Add(label4);
             Controls.Add(button1);
@@ -304,6 +322,7 @@
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox4);
             Controls.Add(pictureBox5);
+            Margin = new Padding(6, 6, 6, 6);
             Name = "Appoiment";
             Text = "Appoiment";
             Load += Appoiment_Load;
