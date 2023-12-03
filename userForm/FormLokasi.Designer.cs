127b64c5cc6ee2cff6b4b3a8b632b6415cc4f5d0
krisnaarianta <krisnaarianta@gmail.com>
2023-12-03 21:03:01 +0700

    fix: translate form lokasi

--- userForm/FormLokasi.Designer.cs
+++ userForm/FormLokasi.Designer.cs
@@ -62,29 +62,32 @@
             // 
             Province.AutoSize = true;
             Province.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            Province.Location = new Point(497, 104);
+            Province.Location = new Point(923, 222);
+            Province.Margin = new Padding(6, 0, 6, 0);
             Province.Name = "Province";
-            Province.Size = new Size(70, 21);
+            Province.Size = new Size(91, 45);
             Province.TabIndex = 1;
-            Province.Text = "Province";
+            Province.Text = "State";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(497, 165);
+            label2.Location = new Point(923, 352);
+            label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(54, 21);
+            label2.Size = new Size(74, 45);
             label2.TabIndex = 3;
-            label2.Text = "Distric";
+            label2.Text = "City";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(497, 224);
+            label3.Location = new Point(923, 478);
+            label3.Margin = new Padding(6, 0, 6, 0);
             label3.Name = "label3";
-            label3.Size = new Size(67, 21);
+            label3.Size = new Size(138, 45);
             label3.TabIndex = 5;
             label3.Text = "Hospital";
             // 
@@ -92,9 +95,10 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(497, 342);
+            label4.Location = new Point(923, 730);
+            label4.Margin = new Padding(6, 0, 6, 0);
             label4.Name = "label4";
-            label4.Size = new Size(57, 21);
+            label4.Size = new Size(117, 45);
             label4.TabIndex = 7;
             label4.Text = "Doctor";
             // 
@@ -102,10 +106,10 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI Black", 16.125F, FontStyle.Bold, GraphicsUnit.Point);
-            label8.Location = new Point(80, 12);
-            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Location = new Point(149, 26);
+            label8.Margin = new Padding(4, 0, 4, 0);
             label8.Name = "label8";
-            label8.Size = new Size(151, 30);
+            label8.Size = new Size(293, 59);
             label8.TabIndex = 19;
             label8.Text = "VirtualCheck";
             // 
@@ -113,9 +117,10 @@
             // 
             pictureBox6.ErrorImage = null;
             pictureBox6.Image = Properties.Resources.VC2;
-            pictureBox6.Location = new Point(12, 12);
+            pictureBox6.Location = new Point(22, 26);
+            pictureBox6.Margin = new Padding(6, 6, 6, 6);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(63, 37);
+            pictureBox6.Size = new Size(117, 79);
             pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox6.TabIndex = 20;
             pictureBox6.TabStop = false;
@@ -123,9 +128,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.hospitallocation;
-            pictureBox1.Location = new Point(12, 55);
+            pictureBox1.Location = new Point(22, 117);
+            pictureBox1.Margin = new Padding(6, 6, 6, 6);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(450, 539);
+            pictureBox1.Size = new Size(836, 1150);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 21;
             pictureBox1.TabStop = false;
@@ -133,9 +139,10 @@
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.Hiasan_3;
-            pictureBox3.Location = new Point(-7, 578);
+            pictureBox3.Location = new Point(-13, 1233);
+            pictureBox3.Margin = new Padding(6, 6, 6, 6);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(265, 195);
+            pictureBox3.Size = new Size(492, 416);
             pictureBox3.TabIndex = 24;
             pictureBox3.TabStop = false;
             // 
@@ -143,9 +150,10 @@
             // 
             pictureBox2.ErrorImage = Properties.Resources.Hiasan_4;
             pictureBox2.Image = Properties.Resources.Hiasan_4;
-            pictureBox2.Location = new Point(570, 401);
+            pictureBox2.Location = new Point(1059, 855);
+            pictureBox2.Margin = new Padding(6, 6, 6, 6);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(315, 287);
+            pictureBox2.Size = new Size(585, 612);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 23;
             pictureBox2.TabStop = false;
@@ -153,9 +161,10 @@
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.Hiasan_1;
-            pictureBox4.Location = new Point(714, -13);
+            pictureBox4.Location = new Point(1326, -28);
+            pictureBox4.Margin = new Padding(6, 6, 6, 6);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(141, 116);
+            pictureBox4.Size = new Size(262, 247);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 25;
             pictureBox4.TabStop = false;
@@ -163,9 +172,10 @@
             // pictureBox5
             // 
             pictureBox5.Image = Properties.Resources.Hiasan_2;
-            pictureBox5.Location = new Point(747, 163);
+            pictureBox5.Location = new Point(1387, 348);
+            pictureBox5.Margin = new Padding(6, 6, 6, 6);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(152, 184);
+            pictureBox5.Size = new Size(282, 393);
             pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox5.TabIndex = 26;
             pictureBox5.TabStop = false;
@@ -174,10 +184,10 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(497, 55);
-            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Location = new Point(923, 117);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(265, 30);
+            label7.Size = new Size(529, 57);
             label7.TabIndex = 27;
             label7.Text = "Location and Your Doctor";
             label7.TextAlign = ContentAlignment.TopCenter;
@@ -190,10 +200,10 @@
             button1.FlatStyle = FlatStyle.Popup;
             button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             button1.ForeColor = SystemColors.Control;
-            button1.Location = new Point(497, 551);
-            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Location = new Point(923, 1175);
+            button1.Margin = new Padding(4, 2, 4, 2);
             button1.Name = "button1";
-            button1.Size = new Size(286, 43);
+            button1.Size = new Size(531, 92);
             button1.TabIndex = 28;
             button1.Text = "Submit";
             button1.UseVisualStyleBackColor = false;
@@ -203,9 +213,10 @@
             // 
             txtProvince.BackColor = SystemColors.Control;
             txtProvince.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtProvince.Location = new Point(497, 128);
+            txtProvince.Location = new Point(923, 273);
+            txtProvince.Margin = new Padding(6, 6, 6, 6);
             txtProvince.Name = "txtProvince";
-            txtProvince.Size = new Size(286, 29);
+            txtProvince.Size = new Size(528, 50);
             txtProvince.TabIndex = 29;
             // 
             // cbDoctor
@@ -213,9 +224,10 @@
             cbDoctor.BackColor = SystemColors.Control;
             cbDoctor.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             cbDoctor.FormattingEnabled = true;
-            cbDoctor.Location = new Point(497, 366);
+            cbDoctor.Location = new Point(923, 781);
+            cbDoctor.Margin = new Padding(6, 6, 6, 6);
             cbDoctor.Name = "cbDoctor";
-            cbDoctor.Size = new Size(286, 29);
+            cbDoctor.Size = new Size(528, 53);
             cbDoctor.TabIndex = 6;
             cbDoctor.TextChanged += cbDoctor_TextChange;
             // 
@@ -224,9 +236,10 @@
             cbHospital.BackColor = SystemColors.Control;
             cbHospital.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             cbHospital.FormattingEnabled = true;
-            cbHospital.Location = new Point(497, 248);
+            cbHospital.Location = new Point(923, 529);
+            cbHospital.Margin = new Padding(6, 6, 6, 6);
             cbHospital.Name = "cbHospital";
-            cbHospital.Size = new Size(286, 29);
+            cbHospital.Size = new Size(528, 53);
             cbHospital.TabIndex = 4;
             cbHospital.TextChanged += cbHospital_TextChange;
             // 
@@ -235,9 +248,10 @@
             cbDistric.BackColor = SystemColors.Control;
             cbDistric.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             cbDistric.FormattingEnabled = true;
-            cbDistric.Location = new Point(497, 189);
+            cbDistric.Location = new Point(923, 403);
+            cbDistric.Margin = new Padding(6, 6, 6, 6);
             cbDistric.Name = "cbDistric";
-            cbDistric.Size = new Size(286, 29);
+            cbDistric.Size = new Size(528, 53);
             cbDistric.TabIndex = 2;
             cbDistric.TextChanged += cbKota_TextChange;
             cbDistric.Click += txtProvince_TextChanged;
@@ -246,9 +260,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(497, 410);
+            label1.Location = new Point(923, 875);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(37, 21);
+            label1.Size = new Size(73, 45);
             label1.TabIndex = 31;
             label1.Text = "Day";
             // 
@@ -257,29 +272,32 @@
             cbDays.BackColor = SystemColors.Control;
             cbDays.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             cbDays.FormattingEnabled = true;
-            cbDays.Location = new Point(497, 434);
+            cbDays.Location = new Point(923, 926);
+            cbDays.Margin = new Padding(6, 6, 6, 6);
             cbDays.Name = "cbDays";
-            cbDays.Size = new Size(286, 29);
+            cbDays.Size = new Size(528, 53);
             cbDays.TabIndex = 30;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(497, 286);
+            label5.Location = new Point(923, 610);
+            label5.Margin = new Padding(6, 0, 6, 0);
             label5.Name = "label5";
-            label5.Size = new Size(70, 21);
+            label5.Size = new Size(153, 45);
             label5.TabIndex = 33;
-            label5.Text = "Spesialis";
+            label5.Text = "Specialist";
             // 
             // cbSpesialis
             // 
             cbSpesialis.BackColor = SystemColors.Control;
             cbSpesialis.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             cbSpesialis.FormattingEnabled = true;
-            cbSpesialis.Location = new Point(497, 310);
+            cbSpesialis.Location = new Point(923, 661);
+            cbSpesialis.Margin = new Padding(6, 6, 6, 6);
             cbSpesialis.Name = "cbSpesialis";
-            cbSpesialis.Size = new Size(286, 29);
+            cbSpesialis.Size = new Size(528, 53);
             cbSpesialis.TabIndex = 32;
             cbSpesialis.TextChanged += cbSpesialis_TextChange;
             // 
@@ -290,19 +308,20 @@
             back.Cursor = Cursors.Hand;
             back.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             back.ForeColor = Color.Firebrick;
-            back.Location = new Point(164, 587);
+            back.Location = new Point(305, 1252);
+            back.Margin = new Padding(6, 0, 6, 0);
             back.Name = "back";
-            back.Size = new Size(44, 21);
+            back.Size = new Size(88, 45);
             back.TabIndex = 34;
             back.Text = "Back";
             back.Click += back_Click;
             // 
             // FormLokasi
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(846, 639);
+            ClientSize = new Size(1571, 1363);
             Controls.Add(back);
             Controls.Add(label5);
             Controls.Add(cbSpesialis);
@@ -325,6 +344,7 @@
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox4);
             Controls.Add(pictureBox5);
+            Margin = new Padding(6, 6, 6, 6);
             Name = "FormLokasi";
             Text = "FormLokasi";
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
